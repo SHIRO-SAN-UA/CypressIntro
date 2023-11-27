9970bf8dfbcb34efa3ce5f58e77524abc8762cd3
Serge K <serge.karpov@gmail.com>
2023-11-27 16:51:35 +0200

    added cypress-mochawesome-reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,13 +2,12 @@ const { defineConfig } = require("cypress");
 
 
 module.exports = defineConfig({
-  projectId: 'zergvi',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
     baseUrl: "https://demo.realworld.io/#/",
-    reporter: "mochawesome",
+    reporter: "cypress-mochawesome-reporter",
   },
   
 });
